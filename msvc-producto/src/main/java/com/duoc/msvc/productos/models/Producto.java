a307b7c3e9c2feee03549182b8b813440ea242ce
Matías Gutiérrez <mat.gutierrezh@duocuc.cl>
2025-05-12 10:51:57 -0400

    Creación producto en models

---
+++ msvc-producto/src/main/java/com/duoc/msvc/productos/models/Producto.java
@@ -0,0 +1,22 @@
+package com.duoc.msvc.productos.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@Table(name = "productos")
+@Getter @Setter @ToString
+@NoArgsConstructor @AllArgsConstructor
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Long idProducto;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "El campo ")
+    private String marca;
+    private
+}
