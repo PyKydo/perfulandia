877ff437639e9f42f167326a8f29c3703c21760d
david <da.larenas@duocuc.cl>
2025-05-13 14:09:44 -0400

    UsuarioController agregue un par de weas rey

--- msvc-usuario/src/main/java/com/duoc/msvc/usuarios/controllers/UsuarioController.java
+++ msvc-usuario/src/main/java/com/duoc/msvc/usuarios/controllers/UsuarioController.java
@@ -1,11 +1,40 @@
 package com.duoc.msvc.usuarios.controllers;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.duoc.msvc.usuarios.models.entities.Usuario;
+import com.duoc.msvc.usuarios.services.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/usuarios")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public ResponseEntity<List <Usuario>> findAll(){
+        List<Usuario> usuarios =this.usuarioService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(usuarios);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> findById(@PathVariable Long id) {
+        Usuario usuario = this.usuarioService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(usuario);
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> save(@Valid @RequestBody Usuario usuario) {
+        Usuario saved = this.usuarioService.save(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
+
 
 }
