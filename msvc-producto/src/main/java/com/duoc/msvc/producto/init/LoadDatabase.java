62585e9d8c877478a100236d6137e549c80f64d5
Vik <vi.mena@duocuc.cl>
2025-06-05 12:15:50 -0400

    se agrego el logger en la prueba unitaria del microservicio producto

--- msvc-producto/src/main/java/com/duoc/msvc/producto/init/LoadDatabase.java
+++ msvc-producto/src/main/java/com/duoc/msvc/producto/init/LoadDatabase.java
@@ -10,6 +10,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.util.Locale;
 
 @Profile("dev")
@@ -31,6 +32,16 @@ public class LoadDatabase implements CommandLineRunner {
         for(int i=0;i<1000;i++){
             Producto producto = new Producto();
             producto.setNombre(faker.commerce().productName());
+            producto.setMarca(faker.company().name());
+            String precioStr = faker.commerce().price(1000, 10000);
+            BigDecimal precio = new BigDecimal(precioStr);
+            producto.setPrecio(precio);
+            producto.setDescripcion(faker.lorem().sentence());
+            producto.setPorcentajeConcentracion(faker.number().randomDouble(0,1,40));
+
+            logger.info("prueba {}",producto.getNombre());
+            producto = productoRepository.save(producto);
+            logger.info("Hola prueba 2 {}",producto);
         }
     }
     }
